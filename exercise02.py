a9c0dbc0a10f8055e130fca7b9954cfc54e383e3
Nick-Challinor <80798999+Nick-Challinor@users.noreply.github.com>
2021-03-31 14:54:17 +1300

    Update exercise02.py

--- exercise02.py
+++ exercise02.py
@@ -1,4 +1,14 @@
 # Exercise Two
 # Write a simple program that finds the value at the nth position in the Fibonacci sequence
 
-print("The value at the 6th postion in the Fibonacci sequence is 8")
+def fibonacci(n):
+    
+    if n <= 2:
+        print("IF: " + str(n))
+        return 1
+    else:
+        print("ELSE: " + str(n))
+        return fibonacci(n-1) + fibonacci(n-2)
+        
+x = fibonacci(0)
+print(x)
