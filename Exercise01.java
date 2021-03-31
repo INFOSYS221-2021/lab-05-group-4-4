8ccaafa67db270251c08856db0b5d2ff53227ac7
abol632 <80133550+abol632@users.noreply.github.com>
2021-03-31 14:44:58 +1300

    Exercise01.java

--- Exercise01.java
+++ Exercise01.java
@@ -7,24 +7,30 @@ public class ExerciseOne {
       System.out.println(ex01.reverseString("Bsuiness School"));
       
       // Uncomment the following statements after finishing Exercise Four
-    //   System.out.println(ex01.isStringPalindrome("hello world"));
-    //   System.out.println(ex01.isStringPalindrome("Dad"));
-    //   System.out.println(ex01.isStringPalindrome("radar"));
+       System.out.println(ex01.isStringPalindrome("hello world"));
+       System.out.println(ex01.isStringPalindrome("Dad"));
+       System.out.println(ex01.isStringPalindrome("radar"));
     }
     
     // Exercise One
     private String reverseString(String word) {
         String revWord = "";
         for (int i = 0; i < word.length(); i++) {
-             //revWord = revWord + word.charAt(word.length() - i - 1);
+            //revWord = revWord + word.charAt(word.length() - i - 1);
             revWord = word.charAt(i) + revWord;
         }
         return revWord;
     }
+
     
     // Exercise Four
     private boolean isStringPalindrome(String word) {
         // Complete this method to deermine if the given word is a palindrome
-        return false;
+       String revWord = "";
+        for (int i = 0; i < word.length(); i++) {
+            //revWord = revWord + word.charAt(word.length() - i - 1);
+            revWord = word.charAt(i) + revWord;
+        }
+        return revWord.equals(word);
     }
 }
