add6e739f2e4c88d7c4ee2d3837e240e1cf7d51f
dmah731 <79950917+dmah731@users.noreply.github.com>
2021-03-31 14:53:49 +1300

    Update exercise03.py

--- exercise03.py
+++ exercise03.py
@@ -2,3 +2,17 @@
 # Write a simple program that finds the number of digits of a given integer value
 
 print("The number of digits for a value of 100 is 3")
+
+#ANSWER
+a_integer = (abs(-100))
+
+
+b_string = str(a_integer)
+
+
+
+length = len(b_string)
+
+
+
+print(length)
